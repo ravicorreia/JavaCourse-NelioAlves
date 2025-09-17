37ff22cd4f650ab65e1f984d1b1cbcc9b4939bfd
Ravi Correia <ravic@ciandt.com>
2025-09-17 15:58:39 -0300

    Refactor: Class 94 refactor

--- src/udemy/javacourse/com/aula94vectorexercise2/application/Program.java
+++ src/udemy/javacourse/com/aula94vectorexercise2/application/Program.java
@@ -3,32 +3,31 @@ package udemy.javacourse.com.aula94vectorexercise2.application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import udemy.javacourse.com.aula94vectorexercise2.entities.Student;
+import udemy.javacourse.com.aula94vectorexercise2.entities.Rent;
 
 public class Program {
-    public static void main(String[] args) {
-        // Locale.setDefault(Locale.US);
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
 
-        int[] rooms = new int[10];
+    Rent[] vect = new Rent[10];
 
-        System.out.println("How many rooms will be rented?");
-        int n = sc.nextInt();
-        Student[] students = new Student[n];
-        sc.next();
-        for (int i = 0; i < students.length; i++) {
-            System.out.printf("Rent #" + (i + 1));
-            System.out.println();
-            System.out.println("Enter student Name:");
-            String studentName = sc.next();
-            System.out.println("Enter student Name:");
-            String studentEmail = sc.next();
-            System.out.println("Wich room the student wants to rent?");
-            int roomToBeRented = sc.nextInt();
-        }
+    System.out.println("How many rooms will be rented?");
+    int n = sc.nextInt();
 
+    for (int i = 1; i < n; i++) {
+      System.out.println("Rent #" + i + ":");
+      System.out.print("Enter student Name:");
+      sc.nextLine();
+      String name = sc.nextLine();
+      System.out.print("Email: ");
+      String email = sc.nextLine();
+      System.out.print("Room: ");
+      int roomNumber = sc.nextInt();
+      vect[roomNumber] = new Rent(name, email);
 
-        sc.close();
     }
+
+    sc.close();
+  }
 }
